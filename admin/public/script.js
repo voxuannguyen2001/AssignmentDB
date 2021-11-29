a45d88021381cb9dc911a4d8ac1b4c0ae939bf4f
khoak20hcmut <94916207+khoak20hcmut@users.noreply.github.com>
2021-11-29 21:44:28 +0700

    Update script.js

--- admin/public/script.js
+++ admin/public/script.js
@@ -52,5 +52,56 @@ $(document).ready(function () {
         });
     });
     //end ajax insert order
+    
+     //ajax insert new record into cart_contain_product
+    $(".alert-insert-ccp").fadeOut();
+    $(".btn-insert-ccp").click(function () {
+        $.ajax({
+            url: DOMAIN + "/cart_contain_product/doInsert_ccp",
+            method: "post",
+            data: {
+                cart_id: $(".input-cart_id").val(),
+                the_user_id: $(".input-user_id").val(),
+                product_id: $(".input-product_id").val(),
+                shop_id: $(".input-shop_id").val(),
+                product_count: $(".input-product_count").val(),
+                saleprice: $(".input-saleprice").val(),
+            },
+            success: function (result) {
+                if (result == "failed") {
+                    $(".alert-insert-ccp").addClass("alert-danger");
+                    $(".alert-insert-ccp-text").text("Failed!! You should fill all input");
+                    $(".alert-insert-ccp").fadeIn();
+                    setTimeout(function () {
+                        $(".alert-insert-ccp").fadeOut();
+                        $(".alert-insert-ccp-text").text("");
+                        $(".alert-insert-ccp").removeClass("alert-danger");
+                    }, 1500);
+
+                } else if (result == "1") {
+                    $(".alert-insert-ccp").fadeIn();
+                    $(".alert-insert-ccp").addClass("alert-success");
+                    $(".alert-insert-ccp-text").text("Success!!");
+                    setTimeout(function () {
+                        $(".alert-insert-ccp").fadeOut();
+                        $(".alert-insert-ccp").removeClass("alert-success");
+                        $(".alert-insert-ccp-text").text("");
+                    }, 1500);
+                } else {
+                    $(".alert-insert-ccp").addClass("alert-danger");
+                    $(".alert-insert-ccp-text").text("Error! Can't add into database");
+                    $(".alert-insert-ccp").fadeIn();
+                    setTimeout(function () {
+                        $(".alert-insert-ccp").fadeOut();
+                        $(".alert-insert-ccp-text").text("");
+                        $(".alert-insert-ccp").removeClass("alert-danger");
+                    }, 1500);
+                }
+                $("#form_insert_ccp")[0].reset();
+            },
+            error: function () { },
+        });
+    });
+    //end ajax insert new record into cart_contain_product
 });
 
