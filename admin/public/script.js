3db2238eba282b8b047483fb5e21ac098fadac7c
unknown <ngkimphuongtrang@gmail.com>
2021-12-03 10:24:30 +0700

    add proc 3.1

--- admin/public/script.js
+++ admin/public/script.js
@@ -1,57 +1,57 @@
 $(document).ready(function () {
   var DOMAIN = "http://localhost/AssignmentDB/admin";
 
-    //ajax insert new order
-    $(".alert-insert-order").fadeOut();
-    $(".btn-insert-order").click(function () {
-        $.ajax({
-            url: DOMAIN + "/Order/doInsertOrder",
-            method: "post",
-            data: {
-                shipping_id: parseInt($(".input-shipping_id").val()),
-                create_date: $(".input-create_date").val(),
-                user_id: parseInt($(".input-user_id").val()),
-                sname: $(".input-sname").val(),
-                sphone_number: $(".input-sphone_number").val(),
-                saddress: $(".input-saddress").val(),
-                status: $(".input-status").val(),
-            },
-            success: function (result) {
-                if (result == "failed") {
-                    $(".alert-insert-order").addClass("alert-danger");
-                    $(".alert-insert-order-text").text("Failed!! You should fill all inputs");
-                    $(".alert-insert-order").fadeIn();
-                    setTimeout(function () {
-                        $(".alert-insert-order").fadeOut();
-                        $(".alert-insert-order-text").text("");
-                        $(".alert-insert-order").removeClass("alert-danger");
-                    }, 1500);
+  //ajax insert new order
+  $(".alert-insert-order").fadeOut();
+  $(".btn-insert-order").click(function () {
+    $.ajax({
+      url: DOMAIN + "/Order/doInsertOrder",
+      method: "post",
+      data: {
+        shipping_id: parseInt($(".input-shipping_id").val()),
+        create_date: $(".input-create_date").val(),
+        user_id: parseInt($(".input-user_id").val()),
+        sname: $(".input-sname").val(),
+        sphone_number: $(".input-sphone_number").val(),
+        saddress: $(".input-saddress").val(),
+        status: $(".input-status").val(),
+      },
+      success: function (result) {
+        if (result == "failed") {
+          $(".alert-insert-order").addClass("alert-danger");
+          $(".alert-insert-order-text").text("Failed!! You should fill all inputs");
+          $(".alert-insert-order").fadeIn();
+          setTimeout(function () {
+            $(".alert-insert-order").fadeOut();
+            $(".alert-insert-order-text").text("");
+            $(".alert-insert-order").removeClass("alert-danger");
+          }, 1500);
 
-                } else if (result == "1") {
-                    $(".alert-insert-order").fadeIn();
-                    $(".alert-insert-order").addClass("alert-success");
-                    $(".alert-insert-order-text").text("Success!!");
-                    setTimeout(function () {
-                        $(".alert-insert-order").fadeOut();
-                        $(".alert-insert-order").removeClass("alert-success");
-                        $(".alert-insert-order-text").text("");
-                    }, 1500);
-                } else {
-                    $(".alert-insert-order").addClass("alert-danger");
-                    $(".alert-insert-order-text").text("Error! Can't add into database");
-                    $(".alert-insert-order").fadeIn();
-                    setTimeout(function () {
-                        $(".alert-insert-order").fadeOut();
-                        $(".alert-insert-order-text").text("");
-                        $(".alert-insert-order").removeClass("alert-danger");
-                    }, 1500);
-                }
-                // $("#form_insert_order")[0].reset();
-            },
-            error: function () { },
-        });
+        } else if (result == "1") {
+          $(".alert-insert-order").fadeIn();
+          $(".alert-insert-order").addClass("alert-success");
+          $(".alert-insert-order-text").text("Success!!");
+          setTimeout(function () {
+            $(".alert-insert-order").fadeOut();
+            $(".alert-insert-order").removeClass("alert-success");
+            $(".alert-insert-order-text").text("");
+          }, 1500);
+        } else {
+          $(".alert-insert-order").addClass("alert-danger");
+          $(".alert-insert-order-text").text("Error! Can't add into database");
+          $(".alert-insert-order").fadeIn();
+          setTimeout(function () {
+            $(".alert-insert-order").fadeOut();
+            $(".alert-insert-order-text").text("");
+            $(".alert-insert-order").removeClass("alert-danger");
+          }, 1500);
+        }
+        // $("#form_insert_order")[0].reset();
+      },
+      error: function () { },
     });
   });
+
   //end ajax insert order
 
   //ajax insert new record into cart_contain_product
@@ -128,31 +128,31 @@ $(document).ready(function () {
             $(".alert-edit-ocp").removeClass("alert-danger");
           }, 1500);
 
-                } else if (result == "1") {
-                    $(".alert-edit-ocp").fadeIn();
-                    $(".alert-edit-ocp").addClass("alert-success");
-                    $(".alert-edit-ocp-text").text("Success!!");
-                    setTimeout(function () {
-                        $(".alert-edit-ocp").fadeOut();
-                        $(".alert-edit-ocp").removeClass("alert-success");
-                        $(".alert-edit-ocp-text").text("");
-                    }, 1500);
-                } else {
-                    $(".alert-edit-ocp").addClass("alert-danger");
-                    $(".alert-edit-ocp-text").text("Error! The values invalid");
-                    $(".alert-edit-ocp").fadeIn();
-                    setTimeout(function () {
-                        $(".alert-edit-ocp").fadeOut();
-                        $(".alert-edit-ocp-text").text("");
-                        $(".alert-edit-ocp").removeClass("alert-danger");
-                    }, 1500);
-                }
-                // $("#form_edit-ocp")[0].reset();
-            },
-            error: function () { },
-        });
+        } else if (result == "1") {
+          $(".alert-edit-ocp").fadeIn();
+          $(".alert-edit-ocp").addClass("alert-success");
+          $(".alert-edit-ocp-text").text("Success!!");
+          setTimeout(function () {
+            $(".alert-edit-ocp").fadeOut();
+            $(".alert-edit-ocp").removeClass("alert-success");
+            $(".alert-edit-ocp-text").text("");
+          }, 1500);
+        } else {
+          $(".alert-edit-ocp").addClass("alert-danger");
+          $(".alert-edit-ocp-text").text("Error! The values invalid");
+          $(".alert-edit-ocp").fadeIn();
+          setTimeout(function () {
+            $(".alert-edit-ocp").fadeOut();
+            $(".alert-edit-ocp-text").text("");
+            $(".alert-edit-ocp").removeClass("alert-danger");
+          }, 1500);
+        }
+        // $("#form_edit-ocp")[0].reset();
+      },
+      error: function () { },
     });
   });
+
   //end edit order_contains_product
 
   //Thang_procedure1
